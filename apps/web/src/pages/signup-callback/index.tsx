9c5122be04d6410052378941020c0188b9ba7e73
Jerome-YOO <ryu9305@gmail.com>
2023-12-03 21:05:12 +0900

    feat: signup-callback route 오류 해소 및 useParams -> useSearchParams로 훅 변경

--- apps/web/src/pages/signup-callback/index.tsx
+++ apps/web/src/pages/signup-callback/index.tsx
@@ -1,25 +1,27 @@
-import { useParams } from 'next/navigation'
+import { useParams, useSearchParams } from 'next/navigation'
 import { useEffect } from 'react'
 import axios from 'axios';
 
 const SignupCallback = () => {
 
-  const params = useParams<{token?: string}>();
+  const params = useSearchParams();
 
   useEffect(() => {
     (async () => {
-
-      if (params?.token) {  
+      if (params.get('token')) {  
         // TODO : token을 회원가입으로 추가
         try {
-          const response = await axios.post('/api/TBD', {token: params.token});
+          // TEST : console.log({ params: params.get('token') })
+          const response = await axios.post('/api/TBD', {token: params.get('token')});
           // TODO : 성공 액션에 대한 UI 처리
         } catch (error) {
           throw error;
         }
       }
-    })()
+    })();
+
   }, [])
+  
   
   return (
     <div>회원가입 중입니다</div>
