d6a9d27d6dd4c472c71e7b799521d64f93c391b1
Jerome-YOO <ryu9305@gmail.com>
2023-12-04 02:26:53 +0900

    env: react.js 에 중복된 lint config 제거

--- packages/config/eslint/react.js
+++ packages/config/eslint/react.js
@@ -1,9 +1,25 @@
 module.exports = {
-    extends: [
-        require.resolve('./base.js'),
-        'plugin:react/recommended',
-        'plugin:react-hooks/recommended'
-    ],
-    rules: {},
-    settings: { react: { version: 'detect' } }
-}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  settings: { react: { version: 'detect' } },
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  plugins: ['react'],
+  extends: [
+    require.resolve('./base.js'),
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+};
