f09d6abf42acb0a96e75190acd8923f4003b3ceb
Jerome-YOO <ryu9305@gmail.com>
2023-12-19 08:37:38 +0900

    add: _app.tsx에 tailwind.css 적용 및 삭제되면 안되는 next.js 필수 파일 추가 props 할당

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,9 +1,17 @@
-const App = ( { Component, pageProps } ) : JSX.Element => {
-    return (
-        <div>
-            <Component />
-        </div>
-    )
-}
+/** 전역 style file import */
+import "@radix-ui/themes/styles.css";
+import "../assets/style/reset.css";
+import "../assets/style/tailwind.css";
+
+import { Theme } from "@radix-ui/themes";
+import { AppProps } from "next/app";
+
+const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+  return (
+    <Theme style={{ height: "100vh" }}>
+      <Component {...pageProps} />
+    </Theme>
+  );
+};
 
 export default App;
