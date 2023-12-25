d39bd32e61c60a61c2d9e8203fdce9eec2417fe2
divinity6 <divinity666@naver.com>
2023-12-25 22:14:17 +0900

    style: step-1 sign-up , 회원가입1단계 퍼블리싱

--- packages/ui/src/components/view/SignupView.tsx
+++ packages/ui/src/components/view/SignupView.tsx
@@ -8,44 +8,53 @@ import './styles/signupView.css';
 export const SignupView = () => {
 
     return (
-      <Form.Root className="FormRoot">
-        <Text as='h2'
-              align='center'
-              weight='bold'
-              style={{
-                color : "#222222" , fontSize : '28px', lineHeight: '38px' }}>
-          회원가입
+      <Form.Root className="w-[360px]">
+        <Text as="p"
+         className="font-pretendard font-medium leading-body1-16 text-body1-16 text-primary-default">
+          1/2
         </Text>
-        <Form.Field className="FormField" name="email">
-          <div style={{ display: 'flex', alignItems: 'baseline', justifyContent: 'space-between' }}>
-            <Form.Label className="FormLabel">이메일</Form.Label>
-          </div>
-          <Form.Control asChild>
-            <input className="Input" type="email" disabled placeholder="UserID@kakao.net" required />
-          </Form.Control>
-          <Form.Message className="FormMessage" match="valueMissing">
-            이메일을 입력해주세요
-          </Form.Message>
-          <Form.Message className="FormMessage" match="typeMismatch">
-            유효한 이메일이 아닙니다
-          </Form.Message>
-        </Form.Field>
-        <Form.Field className="FormField" name="question">
-          <div style={{ display: 'flex', alignItems: 'baseline', justifyContent: 'space-between' }}>
-            <Form.Label className="FormLabel">*닉네임(0/8)</Form.Label>
+        <Text as='h2'
+          className="font-pretendard font-bold leading-headline2-24 text-headline2-24 text-text-normal">
+          닉네임을 입력해주세요
+        </Text>
+        <Text as="p"
+         className="font-pretendard font-normal leading-body1-16 text-body1-16 text-text-alternative">
+          닉네임은 띄어쓰기 없이 한글, 영문, 숫자만 가능합니다
+        </Text>
+        {/*<Form.Field name="email">*/}
+        {/*  <div style={{ display: 'flex', alignItems: 'baseline', justifyContent: 'space-between' }}>*/}
+        {/*    <Form.Label className="FormLabel">이메일</Form.Label>*/}
+        {/*  </div>*/}
+        {/*  <Form.Control asChild>*/}
+        {/*    <input className="Input" type="email" disabled placeholder="UserID@kakao.net" required />*/}
+        {/*  </Form.Control>*/}
+        {/*  <Form.Message className="FormMessage" match="valueMissing">*/}
+        {/*    이메일을 입력해주세요*/}
+        {/*  </Form.Message>*/}
+        {/*  <Form.Message className="FormMessage" match="typeMismatch">*/}
+        {/*    유효한 이메일이 아닙니다*/}
+        {/*  </Form.Message>*/}
+        {/*</Form.Field>*/}
+        <Form.Field className="mt-8" name="question">
+          <div className="flex items-baseline justify-between">
+            <Form.Label className="text-label2-14 leading-label2-14 text-text-normal">닉네임(0/8)</Form.Label>
             <Form.Message className="FormMessage" match="valueMissing">
               Please enter a question
             </Form.Message>
           </div>
           <Form.Control asChild>
-            <input className="Input" required />
+            <input
+              className="border border-solid border-gray-500 leading-label1-16 text-label1-16 placeholder-text-alternative text-text-normal rounded-lg indent-3 h-[48px] w-full"
+              placeholder="닉네임을 입력해주세요."
+              required />
           </Form.Control>
         </Form.Field>
 
-
         <Form.Submit asChild>
-          <button className="Button" style={{ marginTop: 10 }}>
-            Post question
+          <button
+            className="leading-label1-16 text-label1-16 text-center text-white rounded-3xl w-full h-[48px] mt-10 bg-primary-default disabled:bg-gray-100 disabled:text-text-disable"
+            disabled>
+            다음
           </button>
         </Form.Submit>
       </Form.Root>
