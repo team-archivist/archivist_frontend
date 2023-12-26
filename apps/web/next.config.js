355125a8891cc95b8594b843f087d573c368769c
Jerome-YOO <ryu9305@gmail.com>
2023-12-27 08:30:25 +0900

    feat: 애셋을 사용할 수 있도록 변경 및 아이콘 사용을 위한 svgr 추가

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,24 +1,29 @@
 /** next 관련 설정파일입니다 */
-const headers = [
-    { key: 'Cache-Control', value: 'public, max-age=3600' }
-];
+const headers = [{ key: "Cache-Control", value: "public, max-age=3600" }];
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    distDir: 'dist',
-    async headers() {
-        return [
-            {
-                source: '/:path*',
-                headers: [
-                    { key: 'Access-Control-Allow-Origin', value: '*' },
-                    { key: 'Access-Control-Max-Age', value: '1728000' },
-                    { key: 'Access-Control-Allow-Headers', value: 'Content-Type' }
-                ]
-            }
-        ];
-    }
-}
+  reactStrictMode: false,
+  distDir: "dist",
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Max-Age", value: "1728000" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type" },
+        ],
+      },
+    ];
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+};
 
 module.exports = nextConfig;
