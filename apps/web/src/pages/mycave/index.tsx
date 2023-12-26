cab923b2c4d010dc9255853e36cee52985c11c68
Jerome-YOO <ryu9305@gmail.com>
2023-12-27 08:32:35 +0900

    feat: bookmark -> mycave로 라우팅 변경

--- apps/web/src/pages/mycave/index.tsx
+++ apps/web/src/pages/mycave/index.tsx
@@ -2,12 +2,29 @@ import { BaseButtonMain, NavigationBar } from "@archivist/ui";
 import React from "react";
 import styled from "@emotion/styled";
 import Layout from "@components/Layout";
-import { Avatar, Box, Button, Flex, Tabs, Text } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Tabs,
+  Text,
+} from "@radix-ui/themes";
 import VStack from "@components/Stack/VStack";
 import ACTabs from "@components/Tabs/ACTabs";
 import { PlusIcon } from "@radix-ui/react-icons";
 import useBookmarkAddModal from "@components/Modal/useBookmarkAddModal";
 import BookmarkCard from "@components/BookmarkCard";
+import HStack from "@components/Stack/HStack";
+
+import ARCAVE_LOGO from "@assets/icons/logo.svg";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { css } from "@emotion/react";
+
+import Chip from "@components/Chip";
 
 enum BookmarkTab {
   ALL = "전체",
@@ -15,20 +32,54 @@ enum BookmarkTab {
   SAVED = "저장",
 }
 
-const Bookmark = (props: Props) => {
+enum NavigationBarLeftItem {
+  LOGO = "logo",
+  FEED = "feed",
+  MYCAVE = "mycave",
+}
+
+const MycavePage = (props: Props) => {
   const bookmarkAddModal = useBookmarkAddModal();
+  const currentPathname = usePathname();
   return (
     <>
-      <NavigationBar />
+      <NavigationBar
+        currentPath={currentPathname.slice(1)}
+        leftItems={{
+          [NavigationBarLeftItem.LOGO]: (
+            <Link
+              href={"/feed"}
+              css={css`
+                display: flex;
+                align-items: center;
+              `}
+            >
+              <ARCAVE_LOGO />
+            </Link>
+          ),
+          [NavigationBarLeftItem.FEED]: <Link href={"/feed"}>홈피드</Link>,
+          [NavigationBarLeftItem.MYCAVE]: (
+            <Link href={"/mycave"}>마이케이브</Link>
+          ),
+        }}
+        rightItems={{}}
+      />
       <BookmarkLayout>
-        <Flex gap="4">
+        <Flex gap="4" className="my-8">
           <Avatar
             src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
             fallback="A"
             radius="full"
             size="7"
           />
-          <VStack>세봉</VStack>
+          <VStack className={"my-4"} gap="1">
+            <Heading size="5">세봉</Heading>
+            <HStack gap="2">
+              <Chip>외국어</Chip>
+              <Chip>자기계발</Chip>
+              <Chip>취미</Chip>
+            </HStack>
+          </VStack>
         </Flex>
         <ACTabs
           tabsList={Object.values(BookmarkTab)}
@@ -45,7 +96,12 @@ const Bookmark = (props: Props) => {
                 북마크 추가하기 {<PlusIcon />}
               </BaseButtonMain>
             </Flex>
-            <BookmarkCard />
+            <HStack>
+              <BookmarkCard />
+              <BookmarkCard />
+              <BookmarkCard />
+              <BookmarkCard />
+            </HStack>
           </Tabs.Content>
           <Tabs.Content value={BookmarkTab.GROUP}>
             {BookmarkTab.GROUP}
@@ -60,6 +116,6 @@ const Bookmark = (props: Props) => {
   );
 };
 
-export default Bookmark;
+export default MycavePage;
 
 const BookmarkLayout = styled(Layout)``;
