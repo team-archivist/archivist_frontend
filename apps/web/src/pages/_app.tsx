5571dffcb486c05f863d172bd47d5299c9b83e78
Jerome-YOO <ryu9305@gmail.com>
2024-01-29 05:57:55 +0900

    feat: 링크 업데이트 상황에서 토스트 노출되도록 추가

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -3,16 +3,17 @@ import "@radix-ui/themes/styles.css";
 import "@assets/style/reset.css";
 import "@assets/style/common.css";
 import "@assets/style/tailwind.css";
+import "@assets/style/radix.primitives.css";
 
 import { Theme } from "@radix-ui/themes";
 import { AppProps } from "next/app";
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
-    return (
-        <Theme style={{ height: "100vh" }}>
-            <Component {...pageProps} />
-        </Theme>
-    );
+  return (
+    <Theme style={{ height: "100vh" }}>
+      <Component {...pageProps} />
+    </Theme>
+  );
 };
 
 export default App;
