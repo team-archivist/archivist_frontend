f386016420b5c770c07dfe271b7564dbf90fdae9
divinity6 <divinity666@naver.com>
2024-01-28 19:15:17 +0900

    fix: 기본_font-family_변경

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,7 +1,8 @@
 /** 전역 style file import */
 import "@radix-ui/themes/styles.css";
-import "../assets/style/reset.css";
-import "../assets/style/tailwind.css";
+import "@assets/style/reset.css";
+import "@assets/style/common.css";
+import "@assets/style/tailwind.css";
 
 import { Theme } from "@radix-ui/themes";
 import { AppProps } from "next/app";
