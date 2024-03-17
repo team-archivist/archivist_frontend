9bb7cf55e38dbf1a7f5d653deaceabc210094ac1
Jerome-YOO <ryu9305@gmail.com>
2024-03-17 10:43:43 +0900

    add: schema based validation with zod, rhf & fix eslint setup

--- packages/config/eslint/base.js
+++ packages/config/eslint/base.js
@@ -1,20 +1,32 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    env: {
-        es2022: true
-    },
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module'
-    },
-    plugins: [
-        '@typescript-eslint',
-        'unused-imports',
-        'simple-import-sort',
-        'prettier',
-        'unicorn',
-        'import'
+  parser: "@typescript-eslint/parser",
+  env: {
+    es2022: true,
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: [
+    "@typescript-eslint",
+    "unused-imports",
+    "simple-import-sort",
+    "prettier",
+    "unicorn",
+    "import",
+  ],
+  rules: {
+    "no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
     ],
-    rules: {},
-    ignorePatterns: ['node_modules']
-}
+  },
+  ignorePatterns: ["node_modules"],
+};
