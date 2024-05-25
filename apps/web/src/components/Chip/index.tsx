e53693260886dc2b6a3bfdc51e9e2460b8a11959
Jerome-YOO <ryu9305@gmail.com>
2024-05-26 05:57:29 +0900

    add: Chip 컴포넌트 Tag로 변경

--- apps/web/src/components/Chip/index.tsx
+++ apps/web/src/components/Chip/index.tsx
@@ -1,12 +1,27 @@
-import { Badge } from "@radix-ui/themes";
+import { css } from "@emotion/react";
+import { Tag } from "antd";
 import { PropsWithChildren } from "react";
 
+import { PaletteColor } from "@arcave/utils/color";
+
 const Chip = ({ children }: PropsWithChildren) => {
-  // return <div>{children}</div>;
   return (
-    <Badge variant="solid" radius="full" color="gray" size="2">
+    <Tag
+      bordered={false}
+      css={css`
+        display: flex;
+        height: 36px;
+        padding: 0px 16px;
+        justify-content: center;
+        align-items: center;
+        border-radius: 18px;
+        background: ${PaletteColor.Gray[200]};
+
+        font-size: 14px;
+      `}
+    >
       {children}
-    </Badge>
+    </Tag>
   );
 };
 
