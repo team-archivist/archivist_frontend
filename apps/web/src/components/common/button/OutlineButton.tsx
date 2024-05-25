8ded49131ffa17eb6e5e78feea491518fbc8467c
Jerome-YOO <ryu9305@gmail.com>
2024-05-26 06:45:23 +0900

    fix: OutlineButton 컴포넌트 전환

--- apps/web/src/components/common/button/OutlineButton.tsx
+++ apps/web/src/components/common/button/OutlineButton.tsx
@@ -1,6 +1,5 @@
 import { css } from "@emotion/react";
-import { Button } from "@radix-ui/themes";
-import { ButtonProps } from "antd";
+import { ButtonProps, Button as AntdButton } from "antd";
 import { PropsWithChildren } from "react";
 
 import { SemanticColor } from "@arcave/utils/color";
@@ -10,29 +9,36 @@ type Props = ButtonProps & { backgroundColor?: string };
 
 const OutlineButton = ({
   children,
-  size,
   onClick,
   color,
   ...rest
 }: PropsWithChildren<Props>) => {
   return (
-    <Button
-      size={size ?? "2"}
-      variant="solid"
-      radius="full"
+    <AntdButton
       css={css`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        font-size: 12px;
+        height: 32px;
+        padding: 0px 12px;
+
         border-width: 1px;
         border-style: solid;
+        border-radius: 16px;
+        border-color: ${color ?? SemanticColor.Primary.Default};
+
         background-color: transparent;
         color: ${color ?? SemanticColor.Primary.Default};
-        border-color: ${color ?? SemanticColor.Primary.Default};
+
         cursor: pointer;
       `}
       {...(onClick ? { onClick } : {})}
       {...rest}
     >
       {children}
-    </Button>
+    </AntdButton>
   );
 };
 
