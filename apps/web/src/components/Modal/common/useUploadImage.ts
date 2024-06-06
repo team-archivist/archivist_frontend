1500536b875ceb5a87be05361f12df8492a3ac74
Jerome-YOO <ryu9305@gmail.com>
2024-06-07 05:23:04 +0900

    feat: 이미지 공통훅 ready 상태 추가

--- apps/web/src/components/Modal/common/useUploadImage.ts
+++ apps/web/src/components/Modal/common/useUploadImage.ts
@@ -1,25 +1,34 @@
 import { useRef, useState } from "react";
 
-const useUploadImage = (props: Props) => {
+const useUploadImage = () => {
   const [previewImageUrl, setPreviewImageUrl] = useState<string>("");
   const [fileImageBlob, setFileImageBlob] = useState();
+  const [isImageReady, setIsImageReady] = useState(false);
 
   const fileInputRef = useRef<HTMLInputElement>(null);
 
   const handleChangePreviewImageUrl = (url: string) => {
     // setFileImageBlob(value);
+    setIsImageReady(false);
     setPreviewImageUrl(url);
+    setIsImageReady(true);
   };
 
   const handleChangeFileInput = (event) => {
     if (!event.target.files) {
       return;
     }
-    const [fileBlob] = event.target.files;
-    if (fileBlob) {
-      const imageUrl = window.URL.createObjectURL(fileBlob);
-      setPreviewImageUrl(imageUrl);
-      setFileImageBlob(fileBlob);
+    try {
+      setIsImageReady(false);
+      const [fileBlob] = event.target.files;
+      if (fileBlob) {
+        const imageUrl = window.URL.createObjectURL(fileBlob);
+        setPreviewImageUrl(imageUrl);
+        setFileImageBlob(fileBlob);
+      }
+    } finally {
+      // FIXME : Promise 기반으로 변경
+      setIsImageReady(true);
     }
   };
 
@@ -35,6 +44,7 @@ const useUploadImage = (props: Props) => {
     handleChangePreviewImageUrl,
     handleChangeFileInput,
     resetUploadField,
+    isImageReady,
   };
 };
 
