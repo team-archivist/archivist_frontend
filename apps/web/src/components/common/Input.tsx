5ce7b76367606b1453b916fa94c313d3d7142017
Jerome-YOO <ryu9305@gmail.com>
2024-06-07 05:21:01 +0900

    fix: Input 컴포넌트 디렉토리 -> 파일 변경

--- apps/web/src/components/common/Input.tsx
+++ apps/web/src/components/common/Input.tsx
@@ -1,19 +1,19 @@
-import { Input, InputProps } from "antd";
+import { Input as AntdInput, InputProps } from "antd";
 import { Controller, useFormContext } from "react-hook-form";
 
 type Props = { name: string } & Pick<InputProps, "size" | "placeholder">;
 
-const ACInput = ({ name, size = "large", placeholder }: Props) => {
+const Input = ({ name, size = "large", placeholder }: Props) => {
   const { control } = useFormContext();
   return (
     <Controller
       name={name}
       control={control}
       render={({ field }) => (
-        <Input size={size} placeholder={placeholder} {...field} />
+        <AntdInput size={size} placeholder={placeholder} {...field} />
       )}
     />
   );
 };
 
-export default ACInput;
+export default Input;
