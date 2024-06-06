5b4b42216455efe2fd38e54f7709ceae33cf4aad
Jerome-YOO <ryu9305@gmail.com>
2024-06-07 04:08:54 +0900

    fix: 타이포 린팅

--- apps/web/src/utils/typography.ts
+++ apps/web/src/utils/typography.ts
@@ -1,18 +1,18 @@
 export const Typography = {
   Title1: {
-    get ["20"](){
+    ["20"]() {
       const Default = {
-          fontSize : "20px",
-          fontStyle: "normal",
-          fontWeight: 400,
-          lineHeight: "26px" /* 130% */,
-          letterSpacing: "-0.24px",
-        };
+        fontSize: "20px",
+        fontStyle: "normal",
+        fontWeight: 400,
+        lineHeight: "26px" /* 130% */,
+        letterSpacing: "-0.24px",
+      };
       return {
-        Regular: {...Default },
-        SemiBold: {...Default ,fontWeight : 500 }
-      }
-    }
+        Regular: { ...Default },
+        SemiBold: { ...Default, fontWeight: 500 },
+      };
+    },
   },
   Title2: {
     ["17"]: {
@@ -25,16 +25,16 @@ export const Typography = {
       },
     },
   },
-  Body1 : {
+  Body1: {
     ["16"]: {
-      Regular : {
-        fontSize : "16px",
-        fontStyle : "normal",
-        fontWeight : 400,
-        lineHeight : "26px",
-        letterSpacing : "-0.1px",
-      }
-    }
+      Regular: {
+        fontSize: "16px",
+        fontStyle: "normal",
+        fontWeight: 400,
+        lineHeight: "26px",
+        letterSpacing: "-0.1px",
+      },
+    },
   },
   Body2: {
     ["15"]: {
@@ -55,8 +55,8 @@ export const Typography = {
         fontWeight: 400,
         lineHeight: "20px",
         letterSpacing: "0.14px",
-      }
-    }
+      },
+    },
   },
   Caption1: {
     ["12"]: {
