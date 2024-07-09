1933df5946165c57f809a6f07f32377bcba0160e
pgonee <pgonee@gmail.com>
2024-07-10 00:05:57 +0900

    API서버 쿠키 설정을 위해 proxy 설정

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+const PROXY_AP_DESTINATION =
+  process.env.PROXY_AP_DESTINATION || "http://localhost:8080";
+
 const nextConfig = {
   reactStrictMode: false,
   productionBrowserSourceMaps: true,
@@ -7,6 +11,14 @@ const nextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/backend-api/:path*",
+        destination: `${PROXY_AP_DESTINATION}/:path*`,
+      },
+    ];
   },
   /**
    * - next-js 에서 CORS 이슈를 해결하기 위해 Proxy 설정을 사용합니다
